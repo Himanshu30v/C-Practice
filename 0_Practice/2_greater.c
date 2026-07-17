0aa927574f3364b2d63aa8afb2b5fdfbdc61b43f
Himanshu Verma <himanshu30v@gmail.com>
2026-07-17 17:45:13 +0530

    Revise and Practice previous topics

--- 0_Practice/2_greater.c
+++ 0_Practice/2_greater.c
@@ -1,1 +1,32 @@
 // Write a function that returns the maximum of three numbers.
+
+#include<stdio.h>
+
+
+// Display maximum number b/w three numbers.
+int findmax(int a, int b, int c) {
+    if(a >= b && a >= c) {
+        return a;
+    }
+
+    else if(b >= c && b >= a) {
+        return b;
+   
+    }
+    else 
+        return c;
+    
+}
+
+int main() {
+    int a, b, c;
+    
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+   int greater =  findmax(a, b, c);
+    printf("%d is largest number.", greater);
+    
+
+    return 0;
+}

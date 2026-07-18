e728acba0c82e8a628b2a626a0c356f379802a25
Himanshu Verma <himanshu30v@gmail.com>
2026-07-18 21:07:06 +0530

    Practice Recent topics related problems

---
+++ 0_Practice/4_prime.c
@@ -0,0 +1,31 @@
+// Write a function to check whether a number is Prime.
+
+#include<stdio.h>
+
+int isPrime(int n) {
+    int prime = 0;
+    for(int i =2; i<=n; i++) {
+        if(n % i == 0) {
+                prime =1;
+                break;
+        }
+        if(prime == 0) {
+            return 1;
+        }
+
+        else 
+            return 0;
+    }
+   
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    isprime(n);
+
+    return 0;
+}

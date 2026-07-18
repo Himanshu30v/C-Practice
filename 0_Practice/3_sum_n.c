85a35b6c1aeb3a1e6365aa518569e25c0f458912
Himanshu Verma <himanshu30v@gmail.com>
2026-07-18 20:28:20 +0530

    Practice Recent topics related problems

--- 0_Practice/3_sum_n.c
+++ 0_Practice/3_sum_n.c
@@ -1,2 +1,28 @@
 // Write a recursive function to calculate: sum of n numbers
 
+#include<stdio.h>
+
+void sum(int n) {
+    if(n == 0) {
+        return;
+    }
+
+    sum(n-1);
+    printf("%d", n);
+    if(n != n-1) {
+        printf("+");
+    }
+    
+
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    sum(n);
+
+    return 0;
+}

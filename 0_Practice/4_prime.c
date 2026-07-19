9448d0989c8a9f25bfa88ad243a3cc92dc29d277
Himanshu Verma <himanshu30v@gmail.com>
2026-07-19 20:58:57 +0530

    Practice Recent topics related problems

--- 0_Practice/4_prime.c
+++ 0_Practice/4_prime.c
@@ -4,17 +4,26 @@
 
 int isPrime(int n) {
     int prime = 0;
-    for(int i =2; i<=n; i++) {
-        if(n % i == 0) {
-                prime =1;
+    if(n<=1) {
+        printf("%d is not a prime number.", n);
+    }
+    
+    else {
+        for(int i=2; i<n; i++) {
+            
+            if(n%i==0) {
+                 prime = 1;
                 break;
-        }
-        if(prime == 0) {
-            return 1;
-        }
 
+                
+            }
+            
+        }
+        if(prime ==0) {
+            printf("%d is a prime number.", n);
+        }
         else 
-            return 0;
+        printf("%d is not a prime number.", n);
     }
    
 }
@@ -25,7 +34,7 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &n);
 
-    isprime(n);
+    isPrime(n);
 
     return 0;
 }

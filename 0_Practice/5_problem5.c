356c929c93fbaef5ca53539547bd11f58c81095c
Himanshu Verma <himanshu30v@gmail.com>
2026-07-21 15:32:11 +0530

    Practice Problems of recent topics

--- 0_Practice/5_problem5.c
+++ 0_Practice/5_problem5.c
@@ -4,35 +4,34 @@ Income Slab Tax
 2.5 - 5.0L 5%
 5.0L - 10.0L 20%
 Above 10.0L 30%
+Note that there is no tax below 2.5L. Take income amount as an input from the user.
 */
 
 #include <stdio.h>
 
 int main()
 {
-    float income;
+    float income, tax = 0;
 
     printf("Enter your income(in LPA): ");
     scanf("%f", &income);
 
-    if (income < 2.5)
-    {
-        printf("Tax Free!");
+    if(income <= 2.5) {
+        tax = 0;
     }
-    else if (income >= 2.5 && income <= 5)
-    {
-        printf("You have to pay 5%% tax of your income, To pay: %.2f ", income * 0.05);
+    else if(income <=5) {
+        tax = (income - 2.5)* 0.05;
     }
-    else if (income > 5 && income <= 10)
-    {
-        printf("You have to pay 20%% tax of your income, To pay: %.2f", income * 0.2);
+    else if(income <= 10) {
+        tax = (2.5*0.05) + (income - 5)*0.2;
     }
-    else if (income > 10)
-    {
-        printf("You have to pay 30%% tax of your income, To pay: %.2f", income * 0.3);
+    else {
+        tax = (2.5*0.05) + (5*0.2) + (income - 10)*0.3;
     }
-    else
-        printf("Enter valid income!");
+
+    printf("Your income is: %.2f\n", income);
+    printf("You have to pay %.2f ammount as tax.", tax);
+
 
     return 0;
 }

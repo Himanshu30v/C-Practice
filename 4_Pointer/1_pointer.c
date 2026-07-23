e890f3f05c5a3ca9cf6218a949cdd5987921228f
Himanshu Verma <himanshu30v@gmail.com>
2026-07-23 22:26:27 +0530

    Learn Pointer

--- 4_Pointer/1_pointer.c
+++ 4_Pointer/1_pointer.c
@@ -1,20 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     int i = 72;
-    int* j = &i;  // j is a pointer pointing to i
+    int *j = &i; // j is a pointer pointing to i
     int k = 67;
 
-
     //%p showing value in hexadecimal
-    printf("The addres of i is: %p\n", &i);
-    printf("The addres of i is: %p\n", j);
-    printf("The addres of i is: %p\n", &k);
+    printf("The address of i is: %p\n", &i);
+    printf("The address of i is: %p\n", j);
+    printf("The address of i is: %p\n", &k);
 
-    // %u showing value in decimal 
-    printf("The addres of i is: %u\n", &i);
-    printf("The addres of i is: %u\n", j);
-    printf("The addres of i is: %u\n", &k);
+    // %u showing value in decimal
+    printf("The address of i is: %u\n", &i);
+    printf("The address of i is: %u\n", j);
+    printf("The address of i is: %u\n", &k);
+
+    //  '*' for value and '&' for address
+    printf("The value of address j is: %d", *(&i));
 
     return 0;
 }

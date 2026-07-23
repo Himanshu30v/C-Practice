eb5bd0a2d18c375171dffe014b13b984a06ec66c
Himanshu Verma <himanshu30v@gmail.com>
2026-07-23 22:10:48 +0530

    Learn Recursion Function

--- 3_Function/10_pattern.c
+++ 3_Function/10_pattern.c
@@ -24,10 +24,10 @@ void pattern(int n)
 {
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= 2*i-1; j++)
+        for (int j = 1; j <= 2 * i - 1; j++)
         {
-                printf("*");
-            }
-            printf("\n");
+            printf("*");
         }
+        printf("\n");
     }
+}

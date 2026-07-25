7ea0234a49af0972e578f266f950275238679155
Himanshu Verma <himanshu30v@gmail.com>
2026-07-25 20:53:39 +0530

    Learn Pointer

--- 4_Pointer/9_value10x.c
+++ 4_Pointer/9_value10x.c
@@ -1,9 +1,16 @@
 // Write a program to change the value of a variable to ten times its current value
 
-#include<stdio.h>
+#include <stdio.h>
+void tenTimes(int *i)
+{
+    *i = *i * 10;
+}
 
-int main() {
+int main()
+{
     int i = 6;
-    printf("The 10x value of i is: %d", i*10);
+    tenTimes(&i);
+    printf("The value of 10X 'i' is %d\n ", i);
+
     return 0;
 }

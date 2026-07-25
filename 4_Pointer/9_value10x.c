57b4bf697b07f6eeb3f1b1beb40cf741da224b54
Himanshu Verma <himanshu30v@gmail.com>
2026-07-25 20:38:13 +0530

    Learn Recursion Function

---
+++ 4_Pointer/9_value10x.c
@@ -0,0 +1,9 @@
+// Write a program to change the value of a variable to ten times its current value
+
+#include<stdio.h>
+
+int main() {
+    int i = 6;
+    printf("The 10x value of i is: %d", i*10);
+    return 0;
+}

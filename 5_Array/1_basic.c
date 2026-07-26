6b0f0ea9dc56d0a779e6f9a79fb33cb0276f1428
Himanshu Verma <himanshu30v@gmail.com>
2026-07-26 22:16:46 +0530

    Learn Arrays

---
+++ 5_Array/1_basic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks[10];  // reserve space to store 10 integers
+
+    marks[0] = 90;
+    marks[1] = 98;
+    //  We can go all the way till marks[9]
+
+    printf("The marks of '0' and '1' is %d and %d", marks[0], marks[1]);
+    return 0;
+}

7be50f24703c08e84fb7a5cedd5a1fce948a19f9
Himanshu Verma <himanshu30v@gmail.com>
2026-07-27 20:56:29 +0530

    Learn Arrays

--- 5_Array/1_basic.c
+++ 5_Array/1_basic.c
@@ -6,8 +6,9 @@ int main()
 
     marks[0] = 90;
     marks[1] = 98;
-    //  We can go all the way till marks[10]
+    marks[2] = 89;
+    //  We can go all the way till marks[9]
 
-    printf("The marks of '0' and '1' is %d and %d", marks[0], marks[1]);
+    printf("The marks of '0', '1' and '2' is %d, %d and %d", marks[0], marks[1], marks[2]);
     return 0;
 }

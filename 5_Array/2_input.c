81dd44d5b6ff81a51bcf3cbd398f975a5dbe31c4
Himanshu Verma <himanshu30v@gmail.com>
2026-07-27 21:00:34 +0530

    Learn Arrays

---
+++ 5_Array/2_input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int marks[10];
+
+    printf("Enter marks of student 1:");
+    scanf("%d", &marks[0]);
+
+    printf("The marks of Student 1 is: %d", marks[0]);
+    return 0;
+}

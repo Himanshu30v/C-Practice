57a0268a627df3ec12c7fa21a91099dc3923dfaf
Himanshu Verma <himanshu30v@gmail.com>
2026-07-28 20:48:50 +0530

    Learn Arrays

---
+++ 5_Array/3_input1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int marks[5];
+    
+    printf("Enter marks of Hindi: \n");
+    printf("Enter marks of English: \n");
+    printf("Enter marks of Maths: \n");
+    printf("Enter marks of Physics: \n");
+    printf("Enter marks of Chemistry: \n");
+    return 0;
+}

0c48a1d180bc88f77fa41929560f84a4dbf43a90
Himanshu Verma <himanshu30v@gmail.com>
2026-07-28 21:02:20 +0530

    Learn Arrays

--- 5_Array/3_input1.c
+++ 5_Array/3_input1.c
@@ -3,10 +3,24 @@
 int main() {
     int marks[5];
     
-    printf("Enter marks of Hindi: \n");
-    printf("Enter marks of English: \n");
-    printf("Enter marks of Maths: \n");
-    printf("Enter marks of Physics: \n");
-    printf("Enter marks of Chemistry: \n");
+    printf("Enter marks of Hindi: ");
+    scanf("%d", &marks[0]);
+
+    printf("Enter marks of English: ");
+    scanf("%d", &marks[1]);
+
+    printf("Enter marks of Maths: ");
+    scanf("%d", &marks[2]);
+
+    printf("Enter marks of Physics: ");
+    scanf("%d", &marks[3]);
+
+    printf("Enter marks of Chemistry: ");
+    scanf("%d", &marks[4]);
+
+    int sum = marks[0] + marks[1]  + marks[2] + marks[3] +marks[4];
+
+    
+
     return 0;
 }

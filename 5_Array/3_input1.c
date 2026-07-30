298db0ca5ee702a016afd44280ae9acd3a679e42
Himanshu Verma <himanshu30v@gmail.com>
2026-07-30 21:15:58 +0530

    Learn Arrays

--- 5_Array/3_input1.c
+++ 5_Array/3_input1.c
@@ -1,58 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
-{
-    int marks[5];
+int main() {
+    int score[10];
 
-    printf("Enter marks of Hindi: ");
-    scanf("%d", &marks[0]);
+    printf("Enter Score of the student: ");
+    scanf("%d", score[0]);
 
-    printf("Enter marks of English: ");
-    scanf("%d", &marks[1]);
-
-    printf("Enter marks of Maths: ");
-    scanf("%d", &marks[2]);
-
-    printf("Enter marks of Physics: ");
-    scanf("%d", &marks[3]);
-
-    printf("Enter marks of Chemistry: ");
-    scanf("%d", &marks[4]);
-
-    int sum = marks[0] + marks[1] + marks[2] + marks[3] + marks[4];
-    float percentage = sum / 5;
-
-    printf("Total marks obtained by the student out of 100 is: %d\n", sum);
-    printf("Percentage is: %.2f\n", percentage);
-
-    if (percentage > 90)
-    {
-        printf("Pass with Grade 'A+'\n");
-    }
-    else if (percentage <= 90 && percentage >= 80)
-    {
-        printf("Pass with Grade 'A'\n");
-    }
-    else if (percentage < 80 && percentage >= 70)
-    {
-        printf("Pass with Grade 'B");
-    }
-    else if (percentage < 70 && percentage >= 60)
-    {
-        printf("Pass with Grade 'C'\n");
-    }
-
-    else if (percentage < 60 && percentage >= 50)
-    {
-        printf("Pass with Grade 'D'\n");
-    }
-    else if (percentage < 50 && percentage >= 44)
-    {
-        printf("Pass with Grade 'E'\n");
-    }
-    else
-    {
-        printf("Fail\n");
-    }
     return 0;
 }

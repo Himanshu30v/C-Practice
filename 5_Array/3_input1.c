a298b19c538aece4cd43eecc30214c5626947b4a
Himanshu Verma <himanshu30v@gmail.com>
2026-08-03 21:33:06 +0530

    Learn Arrays

--- 5_Array/3_input1.c
+++ 5_Array/3_input1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    int str[10];
+    int tre[10];
 
-    printf("Enter str of the student: ");
-    scanf("%d", str[0]);
+    printf("Enter tre of the student: ");
+    scanf("%d", tre[0]);
 
     
     return 0;

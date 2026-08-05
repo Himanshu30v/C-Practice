64d0da4bc2f29a35bf57c203b86422d2f8e88c6d
Himanshu Verma <himanshu30v@gmail.com>
2026-08-05 21:04:09 +0530

    Learn Arrays

--- 5_Array/3_input1.c
+++ 5_Array/3_input1.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int part[10];
+    int score[10];
 
-    printf("Enter part of the student: ");
-    scanf("%d", part[0]);
+    printf("Enter score of the student: ");
+    scanf("%d", score[0]);
 
     return 0;
 }

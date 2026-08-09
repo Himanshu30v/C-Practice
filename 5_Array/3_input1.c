5c3a1bd0d810620b5608d466f6fdc782aad0e2a3
Himanshu Verma <himanshu30v@gmail.com>
2026-08-09 20:38:34 +0530

    Learn Arrays

--- 5_Array/3_input1.c
+++ 5_Array/3_input1.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int ptr[10];
+    int score[10];
 
-    printf("Enter ptr of the student: ");
-    scanf("%d", ptr[0]);
+    printf("Enter score of the student: ");
+    scanf("%d", score[0]);
 
     
 

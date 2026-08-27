f26f8c20caf964c5074713c9d1592ff690a5f08f
Himanshu Verma <himanshu30v@gmail.com>
2026-08-27 22:02:37 +0530

    Practice questions!

--- 5_Array/1_basic.c
+++ 5_Array/1_basic.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int marks[10];  // reserve space to store 10 integers
+    int score[10];  // reserve space to store 10 integers
 
-    marks[0] = 90;
-    marks[1] = 98;
-    marks[2] = 89;
-    //  We can go all the way till marks[9]
+    score[0] = 90;
+    score[1] = 98;
+    score[2] = 89;
+    //  We can go all the way till score[9]
 
-    printf("The marks of '0', '1' and '2' is %d, %d and %d", marks[0], marks[1], marks[2]);
+    printf("The score of '0', '1' and '2' is %d, %d and %d", score[0], score[1], score[2]);
     return 0;
 }
